ecc681a699e210be3d64e00c88e2b50606dcf249
Rasmus Miikkulainen <rasmus.miikkulainen39@gmail.com>
2026-08-21 09:33:07 +0300

    changed folders again and did hw5 better

--- hw/tunti2/5.py
+++ hw/tunti2/5.py
@@ -3,26 +3,29 @@ b = int(input("enter b: "))
 
 # 1.
 if a >= 100 and b <= 50:
-    print(1)
+    tulos = 1
 else:
-    print(0)
+    tulos = 0
+print(tulos)
 
 # 2. (correct way)
 if (a >= 100 and b <= 50) or (b >= 100 and a <= 50):
-    print(1)
+    tulos = 1
 else:
-    print(0)
+    tulos = 0
+print(tulos)
 
 # 2. (weird way)
 if a >= 100:
     if b <= 50:
-        print(1)
+        tulos = 1
     else:
-        print(0)
+        tulos = 0
 elif b >= 100:
     if a <= 50:
-        print(1)
+        tulos = 1
     else:
-        print(0)
+        tulos = 0
 else:
-    print(0)
+    tulos = 0
+print(tulos)
