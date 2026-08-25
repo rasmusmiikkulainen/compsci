d5d3d585fc4e41f1f761d07df0ca8bf2c08c2210
Rasmus Miikkulainen <rasmus.miikkulainen39@gmail.com>
2026-08-25 22:24:54 +0300

    did hw1 from analysing control structures

---
+++ hw/analysing-control-structures/1.1.py
@@ -0,0 +1,20 @@
+integers = ["a", "b", "c"]
+
+for i in range(len(integers)):
+    while ((n := input(f"enter {integers[i]}: ")).isnumeric() == False
+           and not (n.startswith("-") and n[1:].isnumeric())
+           or n in integers):
+        print("please enter a distinct int")
+    integers[i] = n
+
+a = int(integers[0])
+b = int(integers[1])
+c = int(integers[2])
+
+result = a
+if b > result:
+    result = b
+if c > result:
+    result = c
+
+print(result)
