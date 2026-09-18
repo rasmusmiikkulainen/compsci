1d4edda0529a896e9a5a816981c4ed6946e4d296
Rasmus Miikkulainen <rasmus.miikkulainen39@gmail.com>
2026-09-18 08:46:33 +0300

    oop-concepts example

---
+++ oop-concepts/main.py
@@ -0,0 +1,13 @@
+class Product:
+    current_id = 0
+    def __init__(self, description, price): # instance method
+        self.description = description
+        self.id_num = Product.current_id
+        Product.current_id += 1
+        self.price = price
+    def __str__(self):
+        return f"product {self.description}, id {self.id_num} @ {self.price}"
+
+ps = [Product("thinkpad", 1299.95), Product("mac", 3299.95)] # __init__() called behind scenes
+for p in ps:
+    print(p) # __str__() called behind scenes
